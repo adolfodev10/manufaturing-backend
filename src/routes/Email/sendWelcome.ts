b4a03eddf71355f6924dcfebaf696178c9735b6b
adolfodev10 <adolfomanueldev10@gmail.com>
2026-05-27 00:47:04 +0100

    fix: update route path for sending welcome email

--- src/routes/Email/sendWelcome.ts
+++ src/routes/Email/sendWelcome.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { sendWelcomeEmail } from '../../modules/services/email/emailService';
 
 export const SendWelcomeEmailRoute = async (app: FastifyInstance) => {
-  app.post('/email/send-welcome', async (req, reply) => {
+  app.post('/email/welcome', async (req, reply) => {
     const { to, name, password } = req.body as { to: string; name: string; password: string };
     
     const success = await sendWelcomeEmail(to, name, password);
