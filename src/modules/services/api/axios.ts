91c3434804ab65a1dce9c13db88993266f9296ec
adolfodev10 <adolfomanueldev10@gmail.com>
2026-05-26 12:33:51 +0100

    fix: change database provider from PostgreSQL to MySQL and update axios base URL to local environment

--- src/modules/services/api/axios.ts
+++ src/modules/services/api/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-// const localUrl = "http://localhost:3300"
-const apiUrl = "https://manufaturing-backend.onrender.com";
+const localUrl = "http://localhost:3300"
+// const apiUrl = "https://manufaturing-backend.onrender.com";
 
 export const api = axios.create({
-    baseURL: apiUrl,
+    // baseURL: apiUrl,
+    baseURL: localUrl,
     withCredentials: true
 })
